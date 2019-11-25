132baaf7ddc7d51192e42a4aae65aa0c160e56dd
Caspar Brenneisen <caspar.brenneisen@fadendaten.ch>
2019-11-25 18:22:12 +0100

    Import missing classes

--- app/Http/Controllers/GroupsController.php
+++ app/Http/Controllers/GroupsController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use DB;
 use Illuminate\Http\Request;
 
 class GroupsController extends Controller
--- app/Http/Controllers/ParticipationsController.php
+++ app/Http/Controllers/ParticipationsController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use DB;
 use Illuminate\Http\Request;
 
 class ParticipationsController extends Controller
