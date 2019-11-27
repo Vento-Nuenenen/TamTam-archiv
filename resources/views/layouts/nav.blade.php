28bf271688c4a2af623c12abfac8b357ef4cb7f9
Vento-Nuenenen <vento@pfadi-nuenenen.ch>
2019-11-27 20:39:05 +0100

    Remove old Nav-Item

--- resources/views/layouts/nav.blade.php
+++ resources/views/layouts/nav.blade.php
@@ -37,17 +37,6 @@
                             </a>
                         </div>
                     </li>
-                    <li class="nav-item dropdown">
-                        <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                            Info
-                        </a>
-
-                        <div class="dropdown-menu" aria-labelledby="navbarDropdown">
-                            <a class="dropdown-item {{ Request::is('gratulation/')}}" href="{{ url('/gratulation') }}">
-                                Gratulation
-                            </a>
-                        </div>
-                    </li>
                 </ul>
         @endif
 
