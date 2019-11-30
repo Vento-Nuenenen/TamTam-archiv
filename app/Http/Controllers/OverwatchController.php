1816e7faf80e18cbb5c1c1530f38ce2c8de25fbb
Caspar Brenneisen <caspar.brenneisen@fadendaten.ch>
2019-11-30 14:11:25 +0100

    Implement autogrouper

--- app/Http/Controllers/OverwatchController.php
+++ app/Http/Controllers/OverwatchController.php
@@ -14,14 +14,25 @@ class OverwatchController extends Controller
 
             return view('overwatch.overwatch', ['user' => $user]);
        }else if($request->input('tableorder')  != null){
-            $users = DB::table('participations')->select('*')->get();
+            $users = DB::table('participations')->inRandomOrder()->get();
 
             foreach($users as $user){
-
+				print_r($user);
             }
 
            return view('overwatch.overwatch')->with('message', 'Tischordnung wurde erfolgreich generiert!');
        }else if($request->input('grouping')  != null){
+        	$groups = DB::table('group')->get();
+        	$groups_count = count($groups);
+	        $users = DB::table('participations')->inRandomOrder()->get();
+	        $j = 1;
+
+	        foreach($users as $user){
+	        	if($j <= $groups_count){
+	        		DB::table('participations')->where('id','=', $user->id)->update(['FK_GRP' => $j]);
+		        }
+	        }
+
            return view('overwatch.overwatch')->with('message', 'Gruppen wurden erfolgreich zugeordnet!');
        }else{
            return view('overwatch.overwatch');
