a75ee73bdb94005ed538d4f1b8d5ed952759ee91
Vento-Nuenenen <vento@pfadi-nuenenen.ch>
2020-03-03 22:06:20 +0100

    Refactor PDF a bit

--- app/Http/Controllers/IdentificationPrintController.php
+++ app/Http/Controllers/IdentificationPrintController.php
@@ -16,7 +16,7 @@ class IdentificationPrintController extends Controller
     public function export(Request $request){
 	    // define barcode style
 	    $style = array(
-		    'align' => 'R',
+		    'align' => 'L',
 		    'stretch' => false,
 		    'text' => true,
 	    );
@@ -34,16 +34,22 @@ class IdentificationPrintController extends Controller
 		    PDF::SetMargins(5, 5, 5, true);
 		    PDF::SetAutoPageBreak(true, 5);
 
-		    PDF::AddPage('L', 'A7');
-		    !empty($person->scout_name) ? PDF::MultiCell(0, 0, $person->scout_name, 0, 'L') : PDF::MultiCell(0, 0, 'Kein Pfadiname angegeben', 0, 'L');
-		    PDF::MultiCell(0, 10, $person->first_name . ' ' . $person->last_name, 0, 'L');
-		    PDF::MultiCell(0, 0, $person->address, 0, 'L');
-		    PDF::MultiCell(0, 10, $person->plz . ' ' . $person->place, 0, 'L');
-		    PDF::MultiCell(0, 10, $birthday, 0, 'L');
-		    PDF::MultiCell(0, 0, $person->gender, 0, 'L');
-
-		    !empty($person->logo_file_name) ? PDF::Image(storage_path('/app/public/img/' . $person->logo_file_name), 65, 5, 30) : PDF::MultiCell(0, 60, 'Kein Bild gefunden', 0, 'R');
-		    !empty($person->barcode) ? PDF::write1DBarcode($person->barcode, 'EAN13', '', '50', '', 10, 0.4,$style ,'N') : PDF::MultiCell(0, 0, 'Kein Barcode gefunden', 0, 'R');
+		    PDF::AddPage('P', 'A4');
+		    !empty($person->scout_name) ? PDF::Cell(0, 0, $person->scout_name, '', 0, 'L') : PDF::Cell(0, 0, 'Kein Pfadiname gefunden', '', 0, 'L');
+		    PDF::Ln(5);
+		    !empty($person->first_name) && !empty($person->first_name) ? PDF::Cell(0, 0, $person->first_name . ' ' . $person->last_name, '', 0, 'L') : PDF::Cell(0, 0, 'Kein Vor & Nachname gefunden', '', 0, 'L');
+		    PDF::Ln(10);
+		    !empty($person->address) ? PDF::Cell(0, 0, $person->address, '', 0, 'L') : PDF::Cell(0, 0, 'Keine Adresse gefunden', '', 0, 'L');
+		    PDF::Ln(5);
+		    !empty($person->plz) && !empty($person->place) ? PDF::Cell(0, 0, $person->plz . ' ' . $person->place, '', 0, 'L') : PDF::Cell(0, 0, 'Kein PLZ & Ort gefunden', '', 0, 'L');
+		    PDF::Ln(10);
+		    !empty($birthday) ? PDF::Cell(0, 0, $birthday, '', 0, 'L') : PDF::Cell(0, 0, 'Kein Geburtstag gefunden', '', 0, 'L');
+		    PDF::Ln(10);
+		    !empty($person->gender) ? PDF::Cell(0, 0, $person->gender, '', 0, 'L') : PDF::Cell(0, 0, 'Kein Geschlecht gefunden', '', 0, 'L');
+		    PDF::SetXY(50, 5);
+		    !empty($person->logo_file_name) ? PDF::Image(storage_path('/app/public/img/' . $person->logo_file_name), 65, 5, 30) : PDF::Cell(0, 0, 'Kein Profilbild gefunden', '', 0, 'L');
+		    PDF::SetXY(50, 15);
+		    !empty($person->barcode) ? PDF::write1DBarcode($person->barcode, 'EAN13', '', '40', '', 10, 0.4, $style, 'L') : PDF::Cell(0, 0, 'Kein Barcode gefunden', '', 0, 'L');
 	    }
 
 	    return response(PDF::Output(), 200)->header('Content-Type', 'application/pdf');
