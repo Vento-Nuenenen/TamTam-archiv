864756d95e7f48e41afa5684289a6de67b1d8571
Vento-Nuenenen <vento@pfadi-nuenenen.ch>
2020-03-06 23:14:43 +0100

    Implement Overwatches

--- app/Http/Controllers/OverwatchController.php
+++ app/Http/Controllers/OverwatchController.php
@@ -10,9 +10,32 @@ class OverwatchController extends Controller
     public function index(Request $request){
         if($request->input('barcode') != null){
             $barcode = $request->input('barcode');
-            $user_code = DB::table('participations')->select('participations.*')->where('barcode', '=', $barcode)->first();
+	        $tns = DB::select("SELECT participations.*, points.*, groups.*, GROUP_CONCAT(points.points) AS points, 
+				GROUP_CONCAT(points.is_addition) AS additions FROM `participations`
+  			    LEFT JOIN `points` ON `points`.`FK_PRT` = `participations`.`id`
+  			    LEFT JOIN `groups` ON `participations`.`FK_GRP` = `groups`.`id` WHERE `participations`.`barcode` LIKE $barcode
+ 				GROUP BY participations.id;");
 
-            return view('overwatch.overwatch', ['user' => $user_code]);
+	        foreach($tns as $tn){
+		        $balance = 0;
+
+		        if(!empty($tn->points) || !empty($tn->additions)){
+			        $points = explode(',', $tn->points);
+			        $additions = explode(',', $tn->additions);
+
+			        for($i = 0; $i < count($points); $i++){
+				        if($additions[$i] == 1){
+					        $balance += $points[$i];
+				        }else{
+					        $balance -= $points[$i];
+				        }
+			        }
+		        }
+
+		        $tn->current_balance = $balance;
+	        }
+
+            return view('overwatch.overwatch', ['tn' => $tns[0]]);
        }else if($request->input('tableorder')  != null){
             $users = DB::table('participations')->inRandomOrder()->get();
 
--- resources/views/overwatch/overwatch.blade.php
+++ resources/views/overwatch/overwatch.blade.php
@@ -12,59 +12,86 @@
 			<div class="card-header" id="headingOne">
 				<h5 class="mb-0">
 					<button class="btn btn-link" data-toggle="collapse" data-target="#collapseOne" aria-expanded="true" aria-controls="collapseOne">
-                        Barcode Auslesen
+						Barcode Auslesen
 					</button>
 				</h5>
 			</div>
 			<div id="collapseOne" class="collapse show" aria-labelledby="headingOne" data-parent=".User">
 				<div class="card-body table-responsive">
-                    <form method="post">
-                        @csrf
-                        <input type="number" id="barcode" name="barcode" maxlength="13" autofocus />
-                        <input type="submit" />
-                    </form>
+					<form method="post">
+						@csrf
+						<input type="number" id="barcode" name="barcode" maxlength="13" autofocus />
+						<input type="submit" />
+					</form>
 
-                    @if($user ?? '')
-                        {{ $user->barcode }}
-                    @endif
+					<div class="card-body table-responsive">
+						@if($tn ?? '')
+							<table id="dataTable" class="table table-hover">
+								<tr>
+									<th>Barcode: </th>
+									<td>{{ $tn->barcode }}</td>
+								</tr>
+								<tr>
+									<th>Pfadiname: </th>
+									<td>{{ isset($tn->scout_name) ? $tn->scout_name : 'K.A.' }}</td>
+								</tr>
+								<tr>
+									<th>Vor- & Nachname: </th>
+									<td>{{ $tn->first_name . ' ' . $tn->last_name }}</td>
+								</tr>
+								<tr>
+									<th>Gruppe: </th>
+									<td>{{ $tn->group_name }}</td>
+								</tr>
+								<tr>
+									<th>Sitzplatz: </th>
+									<td>{{ $tn->seat_number }}</td>
+								</tr>
+								<tr>
+									<th>Aktuelle Punkte: </th>
+									<td>{{ $tn->current_balance }}</td>
+								</tr>
+							</table>
+						@endif
+					</div>
 				</div>
 			</div>
 		</div>
 
-        <div class="card User mb-3">
-            <div class="card-header" id="headingTwo">
-                <h5 class="mb-0">
-                    <button class="btn btn-link" data-toggle="collapse" data-target="#collapseTwo" aria-expanded="false" aria-controls="collapseTwo">
-                        Tischordnung erstellen
-                    </button>
-                </h5>
-            </div>
-            <div id="collapseTwo" class="collapse show" aria-labelledby="headingTwo" data-parent=".User">
-                <div class="card-body table-responsive">
-                    <form method="post">
-                        @csrf
-                        <input type="submit" name="tableorder" id="tableorder" class="btn btn-success col-md-12" value="Tischordnung erstellen" />
-                    </form>
-                </div>
-            </div>
-        </div>
+		<div class="card User mb-3">
+			<div class="card-header" id="headingTwo">
+				<h5 class="mb-0">
+					<button class="btn btn-link" data-toggle="collapse" data-target="#collapseTwo" aria-expanded="false" aria-controls="collapseTwo">
+						Tischordnung erstellen
+					</button>
+				</h5>
+			</div>
+			<div id="collapseTwo" class="collapse show" aria-labelledby="headingTwo" data-parent=".User">
+				<div class="card-body table-responsive">
+					<form method="post">
+						@csrf
+						<input onclick="return confirm('Are you sure?')" type="submit" name="tableorder" id="tableorder" class="btn btn-success col-md-12" value="Tischordnung erstellen" />
+					</form>
+				</div>
+			</div>
+		</div>
 
-        <div class="card User mb-3">
-            <div class="card-header" id="headingThree">
-                <h5 class="mb-0">
-                    <button class="btn btn-link" data-toggle="collapse" data-target="#collapseThree" aria-expanded="false" aria-controls="collapseThree">
-                        Gruppen aufteilen
-                    </button>
-                </h5>
-            </div>
-            <div id="collapseThree" class="collapse show" aria-labelledby="headingThree" data-parent=".User">
-                <div class="card-body table-responsive">
-                    <form method="post">
-                        @csrf
-                        <input type="submit" name="grouping" id="grouping" class="btn btn-success col-md-12" value="Gruppen aufteilen" />
-                    </form>
-                </div>
-            </div>
-        </div>
+		<div class="card User mb-3">
+			<div class="card-header" id="headingThree">
+				<h5 class="mb-0">
+					<button class="btn btn-link" data-toggle="collapse" data-target="#collapseThree" aria-expanded="false" aria-controls="collapseThree">
+						Gruppen aufteilen
+					</button>
+				</h5>
+			</div>
+			<div id="collapseThree" class="collapse show" aria-labelledby="headingThree" data-parent=".User">
+				<div class="card-body table-responsive">
+					<form method="post">
+						@csrf
+						<input onclick="return confirm('Are you sure?')" type="submit" name="grouping" id="grouping" class="btn btn-success col-md-12" value="Gruppen aufteilen" />
+					</form>
+				</div>
+			</div>
+		</div>
 	</div>
 @endsection
