f099860c3c41c8f0bd5af0d16a60775c87984bd6
Vento-Nuenenen <vento@pfadi-nuenenen.ch>
2020-04-04 22:26:28 +0200

    Implement helpers

--- app/Helper/Helper.php
+++ app/Helper/Helper.php
@@ -1,38 +1,38 @@
 <?php
 
-    namespace App\Helper;
 
-    use DB;
+namespace App\Helper;
 
-    class Helper{
-	    public static function br2nl($str){
-		    return str_replace('<br />', "\n", $str);
-	    }
 
-	    public static function generateBarcode(){
-		    do{
-			    $barcode = mt_rand(000000000001, 999999999999);
-			    $barcode = Helper::ean13_check_digit($barcode);
-		    }while(DB::table('participations')->where('barcode', $barcode)->exists());
-
-		    return $barcode;
-	    }
-
-	    public static function ean13_check_digit($digits){
-		    //first change digits to a string so that we can access individual numbers
-		    $digits = (string) $digits;
-		    // 1\. Add the values of the digits in the even-numbered positions: 2, 4, 6, etc.
-		    $even_sum = $digits{1} + $digits{3} + $digits{5} + $digits{7} + $digits{9} + $digits{11};
-		    // 2\. Multiply this result by 3.
-		    $even_sum_three = $even_sum * 3;
-		    // 3\. Add the values of the digits in the odd-numbered positions: 1, 3, 5, etc.
-		    $odd_sum = $digits{0} + $digits{2} + $digits{4} + $digits{6} + $digits{8} + $digits{10};
-		    // 4\. Sum the results of steps 2 and 3.
-		    $total_sum = $even_sum_three + $odd_sum;
-		    // 5\. The check character is the smallest number which, when added to the
-		    // result in step 4,  produces a multiple of 10.
-		    $next_ten = (ceil($total_sum / 10)) * 10;
-		    $check_digit = $next_ten - $total_sum;
-		    return $digits.$check_digit;
-	    }
+class Helper{
+    public static function br2nl($str){
+        return str_replace('<br />', "\n", $str);
     }
+
+    public static function generateBarcode(){
+        do{
+            $barcode = mt_rand(000000000001, 999999999999);
+            $barcode = Helper::ean13_check_digit($barcode);
+        }while(DB::table('participations')->where('barcode', $barcode)->exists());
+
+        return $barcode;
+    }
+
+    public static function ean13_check_digit($digits){
+        //first change digits to a string so that we can access individual numbers
+        $digits = (string) $digits;
+        // 1\. Add the values of the digits in the even-numbered positions: 2, 4, 6, etc.
+        $even_sum = $digits{1} + $digits{3} + $digits{5} + $digits{7} + $digits{9} + $digits{11};
+        // 2\. Multiply this result by 3.
+        $even_sum_three = $even_sum * 3;
+        // 3\. Add the values of the digits in the odd-numbered positions: 1, 3, 5, etc.
+        $odd_sum = $digits{0} + $digits{2} + $digits{4} + $digits{6} + $digits{8} + $digits{10};
+        // 4\. Sum the results of steps 2 and 3.
+        $total_sum = $even_sum_three + $odd_sum;
+        // 5\. The check character is the smallest number which, when added to the
+        // result in step 4,  produces a multiple of 10.
+        $next_ten = (ceil($total_sum / 10)) * 10;
+        $check_digit = $next_ten - $total_sum;
+        return $digits.$check_digit;
+    }
+}
--- app/Helper/csv_helper.php
+++ app/Helper/csv_helper.php
@@ -1,47 +1,46 @@
 <?php
-
-	/*
+/*
 	* Read a csv file containing two rows personId and groupId
 	*/
-	function read_file($file){
-		$persons = array();
-		$delimiter = detect_delimiter($file);
+function read_file($file){
+    $persons = array();
+    $delimiter = detect_delimiter($file);
 
-		// Create array from file contents (personID, groupID)
-		$fp = fopen($file, 'rb');
-		while(!feof($fp)) {
-			$line = fgetcsv($fp, null, $delimiter);
+    // Create array from file contents (personID, groupID)
+    $fp = fopen($file, 'rb');
+    while(!feof($fp)) {
+        $line = fgetcsv($fp, null, $delimiter);
 
-			//Only include lines with two columns
-			if(isset($line[0]) > 0 && isset($line[1]) > 0)
-			{
-				$persons[] = $line;
-			}
-		}
+        //Only include lines with two columns
+        if(isset($line[0]) > 0 && isset($line[1]) > 0)
+        {
+            $persons[] = $line;
+        }
+    }
 
-		return $persons;
-	}
+    return $persons;
+}
 
-	/*
-	* Excel and other tools sometimes use other delimeters than comma.
-	* This can be used to detect such cases
-	*/
-	if (!function_exists('detect_delimiter')) {
-		function detect_delimiter($csvFile){
-			$delimiters = array(
-				';' => 0,
-				',' => 0,
-				"\t" => 0,
-				"|" => 0
-			);
+/*
+* Excel and other tools sometimes use other delimeters than comma.
+* This can be used to detect such cases
+*/
+if (!function_exists('detect_delimiter')) {
+    function detect_delimiter($csvFile){
+        $delimiters = array(
+            ';' => 0,
+            ',' => 0,
+            "\t" => 0,
+            "|" => 0
+        );
 
-			$handle = fopen($csvFile, "r");
-			$firstLine = fgets($handle);
-			fclose($handle);
-			foreach ($delimiters as $delimiter => &$count) {
-				$count = count(str_getcsv($firstLine, $delimiter));
-			}
+        $handle = fopen($csvFile, "r");
+        $firstLine = fgets($handle);
+        fclose($handle);
+        foreach ($delimiters as $delimiter => &$count) {
+            $count = count(str_getcsv($firstLine, $delimiter));
+        }
 
-			return array_search(max($delimiters), $delimiters);
-		}
-	}
+        return array_search(max($delimiters), $delimiters);
+    }
+}
