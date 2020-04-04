bb62fd26e9a3a52568011b99f7d14b8e1b9016a5
Vento-Nuenenen <vento@pfadi-nuenenen.ch>
2020-04-04 23:00:15 +0200

    Update opcache.php

--- config/opcache.php
+++ config/opcache.php
@@ -4,6 +4,7 @@ return [
     'url' => env('OPCACHE_URL', config('app.url')),
     'prefix' => 'opcache-api',
     'verify_ssl' => true,
+    'verify_host' => 2, // 0 for disabled
     'headers' => [],
     'directories' => [
         base_path('app'),
@@ -23,6 +24,7 @@ return [
         'Stub',
         'stubs',
         'Stubs',
+        'dumper',
         'Dumper'
     ]
 ];
