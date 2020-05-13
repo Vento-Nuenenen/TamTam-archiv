5cc90def54dda716c201794ef21f62340fa8d06f
Caspar Brenneisen <caspar.brenneisen@fadendaten.ch>
2020-05-13 17:44:32 +0200

    Package upgrade

--- .phpstorm.meta.php
+++ .phpstorm.meta.php
@@ -5,7 +5,7 @@ namespace PHPSTORM_META {
 
    /**
     * PhpStorm Meta file, to provide autocomplete information for PhpStorm
-    * Generated on 2020-05-06 19:24:40.
+    * Generated on 2020-05-13 15:38:12.
     *
     * @author Barry vd. Heuvel <barryvdh@gmail.com>
     * @see https://github.com/barryvdh/laravel-ide-helper
