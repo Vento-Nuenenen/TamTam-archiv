498edbb54e7c79c82c85b529ec564027dc0f08d9
Vento-Nuenenen <vento@pfadi-nuenenen.ch>
2020-05-25 21:30:47 +0200

    Install airbrake

--- _ide_helper.php
+++ _ide_helper.php
@@ -3,7 +3,7 @@
 
 /**
  * A helper file for Laravel, to provide autocomplete information to your IDE
- * Generated for Laravel 7.12.0 on 2020-05-25 19:21:36.
+ * Generated for Laravel 7.12.0 on 2020-05-25 19:30:41.
  *
  * This file should not be included in your code, only analyzed by your IDE!
  *
