db86769770a73d7157b08956b588033181480d52
Vento-Nuenenen <vento@pfadi-nuenenen.ch>
2020-06-01 13:02:26 +0200

    Package upgrade

--- .phpstorm.meta.php
+++ .phpstorm.meta.php
@@ -5,7 +5,7 @@ namespace PHPSTORM_META {
 
    /**
     * PhpStorm Meta file, to provide autocomplete information for PhpStorm
-    * Generated on 2020-05-25 19:30:41.
+    * Generated on 2020-06-01 11:02:15.
     *
     * @author Barry vd. Heuvel <barryvdh@gmail.com>
     * @see https://github.com/barryvdh/laravel-ide-helper
