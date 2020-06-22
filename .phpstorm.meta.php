6579dfd9e00d324575250f60120c9be4b5f880e9
Vento-Nuenenen <vento@pfadi-nuenenen.ch>
2020-06-22 20:30:29 +0200

    Package upgrade

--- .phpstorm.meta.php
+++ .phpstorm.meta.php
@@ -5,13 +5,14 @@ namespace PHPSTORM_META {
 
    /**
     * PhpStorm Meta file, to provide autocomplete information for PhpStorm
-    * Generated on 2020-06-17 15:43:43.
+    * Generated on 2020-06-22 18:01:31.
     *
     * @author Barry vd. Heuvel <barryvdh@gmail.com>
     * @see https://github.com/barryvdh/laravel-ide-helper
     */
     override(new \Illuminate\Contracts\Container\Container, map([
         '' => '@',
+        'Airbrake\Notifier' => \Airbrake\Notifier::class,
         'Asm89\Stack\CorsService' => \Asm89\Stack\CorsService::class,
         'Barryvdh\Debugbar\LaravelDebugbar' => \Barryvdh\Debugbar\LaravelDebugbar::class,
         'DNS1D' => \Milon\Barcode\DNS1D::class,
@@ -174,6 +175,7 @@ namespace PHPSTORM_META {
     ]));
     override(\Illuminate\Container\Container::makeWith(0), map([
         '' => '@',
+        'Airbrake\Notifier' => \Airbrake\Notifier::class,
         'Asm89\Stack\CorsService' => \Asm89\Stack\CorsService::class,
         'Barryvdh\Debugbar\LaravelDebugbar' => \Barryvdh\Debugbar\LaravelDebugbar::class,
         'DNS1D' => \Milon\Barcode\DNS1D::class,
@@ -336,6 +338,7 @@ namespace PHPSTORM_META {
     ]));
     override(\Illuminate\Contracts\Container\Container::make(0), map([
         '' => '@',
+        'Airbrake\Notifier' => \Airbrake\Notifier::class,
         'Asm89\Stack\CorsService' => \Asm89\Stack\CorsService::class,
         'Barryvdh\Debugbar\LaravelDebugbar' => \Barryvdh\Debugbar\LaravelDebugbar::class,
         'DNS1D' => \Milon\Barcode\DNS1D::class,
@@ -498,6 +501,7 @@ namespace PHPSTORM_META {
     ]));
     override(\Illuminate\Contracts\Container\Container::makeWith(0), map([
         '' => '@',
+        'Airbrake\Notifier' => \Airbrake\Notifier::class,
         'Asm89\Stack\CorsService' => \Asm89\Stack\CorsService::class,
         'Barryvdh\Debugbar\LaravelDebugbar' => \Barryvdh\Debugbar\LaravelDebugbar::class,
         'DNS1D' => \Milon\Barcode\DNS1D::class,
@@ -660,6 +664,7 @@ namespace PHPSTORM_META {
     ]));
     override(\App::make(0), map([
         '' => '@',
+        'Airbrake\Notifier' => \Airbrake\Notifier::class,
         'Asm89\Stack\CorsService' => \Asm89\Stack\CorsService::class,
         'Barryvdh\Debugbar\LaravelDebugbar' => \Barryvdh\Debugbar\LaravelDebugbar::class,
         'DNS1D' => \Milon\Barcode\DNS1D::class,
@@ -822,6 +827,7 @@ namespace PHPSTORM_META {
     ]));
     override(\App::makeWith(0), map([
         '' => '@',
+        'Airbrake\Notifier' => \Airbrake\Notifier::class,
         'Asm89\Stack\CorsService' => \Asm89\Stack\CorsService::class,
         'Barryvdh\Debugbar\LaravelDebugbar' => \Barryvdh\Debugbar\LaravelDebugbar::class,
         'DNS1D' => \Milon\Barcode\DNS1D::class,
@@ -984,6 +990,7 @@ namespace PHPSTORM_META {
     ]));
     override(\app(0), map([
         '' => '@',
+        'Airbrake\Notifier' => \Airbrake\Notifier::class,
         'Asm89\Stack\CorsService' => \Asm89\Stack\CorsService::class,
         'Barryvdh\Debugbar\LaravelDebugbar' => \Barryvdh\Debugbar\LaravelDebugbar::class,
         'DNS1D' => \Milon\Barcode\DNS1D::class,
@@ -1146,6 +1153,7 @@ namespace PHPSTORM_META {
     ]));
     override(\resolve(0), map([
         '' => '@',
+        'Airbrake\Notifier' => \Airbrake\Notifier::class,
         'Asm89\Stack\CorsService' => \Asm89\Stack\CorsService::class,
         'Barryvdh\Debugbar\LaravelDebugbar' => \Barryvdh\Debugbar\LaravelDebugbar::class,
         'DNS1D' => \Milon\Barcode\DNS1D::class,
--- _ide_helper.php
+++ _ide_helper.php
@@ -3,7 +3,7 @@
 
 /**
  * A helper file for Laravel, to provide autocomplete information to your IDE
- * Generated for Laravel 7.16.1 on 2020-06-17 15:43:45.
+ * Generated for Laravel 7.16.1 on 2020-06-22 18:01:31.
  *
  * This file should not be included in your code, only analyzed by your IDE!
  *
