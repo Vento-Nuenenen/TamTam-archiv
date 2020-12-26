d5cfe96ad6dba287de64dc4189d562414b7d8a7e
Vento <vento@pfadi-nuenenen.ch>
2020-12-26 20:00:09 +0100

    Create EmergencyNumbers model

---
+++ app/Models/EmergencyNumbers.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class EmergencyNumbers extends Model
+{
+    protected $table = 'emergency_numbers';
+
+    protected $fillable = [
+        'name',
+        'number',
+        'order'
+    ];
+}
