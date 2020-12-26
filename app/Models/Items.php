b435a8de2b524dbe2de71d732c372f12d7ac3cbd
Vento <vento@pfadi-nuenenen.ch>
2020-12-26 21:49:44 +0100

    Implement items model

---
+++ app/Models/Items.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Items extends Model
+{
+    protected $table = 'items';
+
+    protected $fillable = [
+        'item_name',
+        'item_price',
+        'item_barcode'
+    ];
+}
