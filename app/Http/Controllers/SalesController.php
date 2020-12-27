f32035c66ee4b9664326581ddeaaaf15fc0b6e49
Vento <vento@pfadi-nuenenen.ch>
2020-12-27 03:23:25 +0100

    Update SalesController.php

--- app/Http/Controllers/SalesController.php
+++ app/Http/Controllers/SalesController.php
@@ -5,8 +5,11 @@ namespace App\Http\Controllers;
 use App\Models\Item;
 use App\Models\Participant;
 use Illuminate\Contracts\Foundation\Application;
+use Illuminate\Contracts\Routing\ResponseFactory;
 use Illuminate\Contracts\View\Factory;
 use Illuminate\Contracts\View\View;
+use Illuminate\Http\RedirectResponse;
+use Illuminate\Http\Request;
 use Illuminate\Http\Response;
 
 class SalesController extends Controller
@@ -22,5 +25,19 @@ class SalesController extends Controller
         $items = Item::all();
 
         return view('sales.sales', ['participations' => $participations, 'items' => $items]);
+    }
+
+    /**
+     * Lookup EAN Codes.
+     *
+     * @param Request $request
+     *
+     * @return Application|ResponseFactory|RedirectResponse|Response
+     */
+    public function lookup(Request $request)
+    {
+        $item = Item::where('item_barcode', '=', $request->ean)->get();
+
+        return response("Test", 200);
     }
 }
