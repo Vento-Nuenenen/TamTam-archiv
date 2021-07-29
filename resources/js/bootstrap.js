cd3390e4621ec018472a1b419af7b5a56afe4160
Caspar Brenneisen <caspar.brenneisen@fadendaten.ch>
2021-07-30 00:06:22 +0200

    Cleanup

--- resources/js/bootstrap.js
+++ resources/js/bootstrap.js
@@ -17,12 +17,6 @@ window.axios = require('axios');
 
 window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
 
-/**
- * Echo exposes an expressive API for subscribing to channels and listening
- * for events that are broadcast by Laravel. Echo and event broadcasting
- * allows your team to easily build robust real-time web applications.
- */
-
 let token = document.head.querySelector('meta[name="csrf-token"]');
 
 if (token) {
