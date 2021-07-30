a6b80aed08bbecb41d96af021b2793be44154ff0
Caspar Brenneisen <caspar.brenneisen@fadendaten.ch>
2021-07-30 22:22:25 +0200

    Update airbrake.php

--- config/airbrake.php
+++ config/airbrake.php
@@ -8,11 +8,11 @@ return [
 
     //leave the following options empty to use defaults
 
-    'host'          => null, //airbrake api host e.g.: 'api.airbrake.io' or 'http://errbit.example.com
+    'host'          => null, #airbrake api host e.g.: 'api.airbrake.io' or 'http://errbit.example.com
     'appVersion'    => null,
-    'revision'      => null, //git revision
+    'revision'      => null, #git revision
     'rootDirectory' => null,
-    'keysBlacklist' => null, //list of keys containing sensitive information that must be filtered out
-    'httpClient'    => null, //http client implementing GuzzleHttp\ClientInterface
+    'keysBlacklist' => null, #list of keys containing sensitive information that must be filtered out
+    'httpClient'    => null, #http client implementing GuzzleHttp\ClientInterface
 
 ];
