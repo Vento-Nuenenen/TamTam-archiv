93ef2ee3cc6351ad5220ff7bb7d37046fc0c3a86
Caspar Brenneisen <caspar.brenneisen@fadendaten.ch>
2021-07-30 22:54:27 +0200

    Update PrintGratulationController.php

--- app/Http/Controllers/PrintGratulationController.php
+++ app/Http/Controllers/PrintGratulationController.php
@@ -2,13 +2,21 @@
 
 namespace App\Http\Controllers;
 
-use App\Helper\Helper;
-use DB;
+use Illuminate\Contracts\Foundation\Application;
+use Illuminate\Contracts\View\Factory;
+use Illuminate\Contracts\View\View;
 use Illuminate\Http\Request;
+use Illuminate\Http\Response;
+use Illuminate\Support\Facades\DB;
 use PDF;
 
 class PrintGratulationController extends Controller
 {
+    /**
+     * Display a listing of the resource.
+     *
+     * @return Application|Factory|View
+     */
     public function index()
     {
         if (! file_exists(storage_path('app/template/gratulation.txt'))) {
