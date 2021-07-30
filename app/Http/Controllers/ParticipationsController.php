d0e46e3eb8cd7af4cb5c47b29497885e436f1fab
Caspar Brenneisen <caspar.brenneisen@fadendaten.ch>
2021-07-30 22:35:09 +0200

    Update ParticipationsController.php

--- app/Http/Controllers/ParticipationsController.php
+++ app/Http/Controllers/ParticipationsController.php
@@ -2,15 +2,14 @@
 
 namespace App\Http\Controllers;
 
-use App\Helper\Helper;
-use App\Models\Participant;
 use Carbon\Carbon;
-use DB;
+use Illuminate\Contracts\Foundation\Application;
 use Illuminate\Contracts\View\Factory;
+use Illuminate\Contracts\View\View;
 use Illuminate\Http\RedirectResponse;
 use Illuminate\Http\Request;
 use Illuminate\Http\Response;
-use Illuminate\View\View;
+use Illuminate\Support\Facades\DB;
 
 class ParticipationsController extends Controller
 {
@@ -19,7 +18,7 @@ class ParticipationsController extends Controller
      *
      * @param Request $request
      *
-     * @return Factory|View
+     * @return Application|Factory|View
      */
     public function index(Request $request)
     {
@@ -45,7 +44,7 @@ class ParticipationsController extends Controller
     /**
      * Show the form for creating a new resource.
      *
-     * @return Factory|View
+     * @return Application|Factory|View
      */
     public function create()
     {
@@ -113,6 +112,8 @@ class ParticipationsController extends Controller
     }
 
     /**
+     * Import participants from csv
+     *
      * @param Request $request
      * @return RedirectResponse
      */
@@ -164,11 +165,11 @@ class ParticipationsController extends Controller
     /**
      * Show the form for editing the specified resource.
      *
-     * @param $pid
+     * @param int $pid
      *
-     * @return Factory|View
+     * @return Application|Factory|View
      */
-    public function edit($pid)
+    public function edit(int $pid)
     {
         $participations = DB::table('participations')->where('id', '=', $pid)->first();
         $groups = DB::table('groups')->select('groups.id', 'groups.group_name')->get();
@@ -180,11 +181,11 @@ class ParticipationsController extends Controller
      * Update the specified resource in storage.
      *
      * @param Request $request
-     * @param         $pid
+     * @param int $pid
      *
-     * @return RedirectResponse|Response
+     * @return RedirectResponse
      */
-    public function update(Request $request, $pid)
+    public function update(Request $request, int $pid)
     {
         $scout_name = $request->input('scout_name');
         $first_name = $request->input('first_name');
@@ -241,13 +242,13 @@ class ParticipationsController extends Controller
     /**
      * Remove the specified resource from storage.
      *
-     * @param $uid
+     * @param int $pid
      *
-     * @return Response
+     * @return RedirectResponse
      */
-    public function destroy($uid)
+    public function destroy(int $pid)
     {
-        DB::table('participations')->where('id', '=', $uid)->delete();
+        DB::table('participations')->where('id', '=', $pid)->delete();
 
         return redirect()->back()->with('message', 'Teilnehmer erfolgreich gelöscht.');
     }
