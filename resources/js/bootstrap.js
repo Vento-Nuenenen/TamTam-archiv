8c0c80777467b4e65a9cd9e4542e3316643205b3
Caspar Brenneisen <caspar.brenneisen@fadendaten.ch>
2021-07-30 15:32:05 +0200

    Update bootstrap.js

--- resources/js/bootstrap.js
+++ resources/js/bootstrap.js
@@ -1,11 +1,17 @@
 window._ = require('lodash');
 
+/**
+ * We'll load jQuery and the Bootstrap jQuery plugin which provides support
+ * for JavaScript based Bootstrap features such as modals and tabs. This
+ * code may be modified to fit the specific needs of your application.
+ */
+
 try {
-    window.$ = window.jQuery = require('jquery');
     window.Popper = require('popper.js').default;
+    window.$ = window.jQuery = require('jquery');
 
     require('bootstrap');
-}catch (e) {}
+} catch (e) {}
 
 /**
  * We'll load the axios HTTP library which allows us to easily issue requests
@@ -16,14 +22,6 @@ try {
 window.axios = require('axios');
 
 window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
-
-let token = document.head.querySelector('meta[name="csrf-token"]');
-
-if (token) {
-    window.axios.defaults.headers.common['X-CSRF-TOKEN'] = token.content;
-} else {
-    console.error('CSRF token not found: https://laravel.com/docs/csrf#csrf-x-csrf-token');
-}
 
 require('bootstrap4-toggle');
 require('bootstrap-select');
