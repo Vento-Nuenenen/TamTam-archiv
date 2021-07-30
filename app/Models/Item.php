1b751d6a1f2f0f40545a833a345c59797a979365
Caspar Brenneisen <caspar.brenneisen@fadendaten.ch>
2021-07-30 23:17:17 +0200

    Update Item.php

--- app/Models/Item.php
+++ app/Models/Item.php
@@ -2,10 +2,13 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Item extends Model
 {
+    use HasFactory;
+
     protected $table = 'items';
 
     protected $fillable = [
