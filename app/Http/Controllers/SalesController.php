d4575f19d5f5313b9999dc136cd7badee03fba66
Vento <vento@pfadi-nuenenen.ch>
2021-08-08 14:58:35 +0200

    Package update

--- app/Http/Controllers/SalesController.php
+++ app/Http/Controllers/SalesController.php
@@ -17,7 +17,7 @@ class SalesController extends Controller
     /**
      * Display a listing of the resource.
      *
-     * @return Application|Factory|View|Response
+     * @return Application|Factory|View
      */
     public function index()
     {
@@ -32,7 +32,7 @@ class SalesController extends Controller
      *
      * @param Request $request
      *
-     * @return Application|ResponseFactory|RedirectResponse|Response
+     * @return Application|Response|ResponseFactory
      */
     public function lookup(Request $request)
     {
