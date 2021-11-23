b43ef97a5d5235656b87967bec377882148d5164
Shift <shift@laravelshift.com>
2021-11-23 17:33:11 +0000

    Configure build for Tailwind assets

--- webpack.mix.js
+++ webpack.mix.js
@@ -14,3 +14,9 @@ const mix = require('laravel-mix');
 mix.js('resources/js/app.js', 'public/js')
     .sass('resources/sass/app.scss', 'public/css')
     .sourceMaps();
+
+
+mix.postCss('resources/css/tailwind.css', 'public/css/app.css', [
+    require('postcss-import'),
+    require('tailwindcss'),
+]);
