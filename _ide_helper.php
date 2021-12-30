d65247d1005f5024668b7a0552b00997d37e399a
Vento <vento@pfadi-nuenenen.ch>
2021-12-30 15:39:00 +0100

    Package upgrade

--- _ide_helper.php
+++ _ide_helper.php
@@ -16045,7 +16045,7 @@
      
 }
 
-        namespace Barryvdh\Debugbar { 
+        namespace Barryvdh\Debugbar\Facades { 
             /**
      * 
      *
@@ -16060,7 +16060,7 @@
      * @method static void warning(mixed $message)
      * @see \Barryvdh\Debugbar\LaravelDebugbar
      */ 
-        class Facade {
+        class Debugbar {
                     /**
          * Enable the Debugbar and boot, if not already booted.
          *
@@ -21760,7 +21760,7 @@ namespace  {
             class URL extends \Illuminate\Support\Facades\URL {}
             class Validator extends \Illuminate\Support\Facades\Validator {}
             class View extends \Illuminate\Support\Facades\View {}
-            class Debugbar extends \Barryvdh\Debugbar\Facade {}
+            class Debugbar extends \Barryvdh\Debugbar\Facades\Debugbar {}
             class PDF extends \Elibyy\TCPDF\Facades\TCPDF {}
             class Flare extends \Facade\Ignition\Facades\Flare {}
             class Form extends \Collective\Html\FormFacade {}
