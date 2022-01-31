0c2dc88e4b19bb4dd182c482e00195c7a860de19
Vento <vento@pfadi-nuenenen.ch>
2022-01-31 01:25:59 +0100

    Create GroupsTableSeeder.php

---
+++ database/seeders/GroupsTableSeeder.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace Database\Seeders;
+
+use DB;
+use Illuminate\Database\Seeder;
+
+class GroupsTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        DB::table('groups')->insert([
+            ['id' => '1', 'group_name' => 'Migros'],
+            ['id' => '2', 'group_name' => 'Coop'],
+        ]);
+    }
+}
