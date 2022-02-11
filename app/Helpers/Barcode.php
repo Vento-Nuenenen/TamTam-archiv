869ad5e0ea5ffc2236f5243402c53451f82be33f
Vento <vento@pfadi-nuenenen.ch>
2022-02-11 23:07:34 +0100

    Update Barcode.php

--- app/Helpers/Barcode.php
+++ app/Helpers/Barcode.php
@@ -2,22 +2,17 @@
 
 namespace App\Helpers;
 
-use DB;
-
-
 class Barcode{
-    public static function generateBarcode()
-    {
-        do {
+    public static function generateBarcode(){
+        do{
             $barcode = (string) mt_rand(100000000000, 999999999999);
             $barcode = self::ean13_check_digit($barcode);
-        } while (DB::table('participations')->where('barcode', $barcode)->exists());
+        }while(\DB::table('participations')->where('barcode', $barcode)->exists());
 
         return $barcode;
     }
 
-    public static function ean13_check_digit($digits)
-    {
+    public static function ean13_check_digit($digits){
         //first change digits to a string so that we can access individual numbers
         $digits = (string) $digits;
         // 1\. Add the values of the digits in the even-numbered positions: 2, 4, 6, etc.
