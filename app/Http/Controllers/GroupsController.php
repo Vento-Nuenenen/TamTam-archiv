b8f140fbd37d2363a1f6fbbae26fa371b017d105
Caspar Brenneisen <caspar.brenneisen@fadendaten.ch>
2022-02-23 08:39:23 +0100

    Fix groups

--- app/Http/Controllers/GroupsController.php
+++ app/Http/Controllers/GroupsController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Group;
 use Illuminate\Contracts\Foundation\Application;
 use Illuminate\Contracts\View\Factory;
 use Illuminate\Contracts\View\View;
