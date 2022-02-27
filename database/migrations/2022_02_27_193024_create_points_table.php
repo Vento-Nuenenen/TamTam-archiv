5811c383c814683db322498c61900c766c703b79
Vento <vento@pfadi-nuenenen.ch>
2022-02-27 20:36:46 +0100

    Create 2022_02_27_193024_create_points_table.php

---
+++ database/migrations/2022_02_27_193024_create_points_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('points', function (Blueprint $table) {
+            $table->id();
+            $table->string('reason')->nullable();
+            $table->integer('points');
+            $table->boolean('is_addition');
+            $table->foreignId('FK_PRT');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('points');
+    }
+};
