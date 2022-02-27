e8c6ffad2ad45bbd4185e654a7c0d60db711d70b
Vento <vento@pfadi-nuenenen.ch>
2022-02-27 20:36:37 +0100

    Create 2022_02_27_193004_create_participations_table.php

---
+++ database/migrations/2022_02_27_193004_create_participations_table.php
@@ -0,0 +1,44 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('participations', function (Blueprint $table) {
+            $table->id();
+            $table->string('scout_name')->nullable();
+            $table->string('first_name');
+            $table->string('last_name');
+            $table->string('address');
+            $table->string('plz');
+            $table->string('place');
+            $table->date('birthday');
+            $table->string('gender')->nullable();
+            $table->string('image')->nullable();
+            $table->string('barcode')->unique()->nullable();
+            $table->integer('seat_number')->nullable();
+            $table->boolean('course_passed')->default(false);
+            $table->foreignId('FK_GRP')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('participations');
+    }
+};
