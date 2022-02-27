e63c7ae965d6eddff934afd207be2eb78757eee5
Vento <vento@pfadi-nuenenen.ch>
2022-02-27 20:43:40 +0100

    Update DatabaseSeeder.php

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,7 +2,7 @@
 
 namespace Database\Seeders;
 
-use App\Http\Controllers\ParticipationsController;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
