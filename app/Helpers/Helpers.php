a211cc0306fe0e47e18bc6b41b7b9ce10f94e6b9
Vento <vento@pfadi-nuenenen.ch>
2022-02-27 20:24:58 +0100

    Update Helpers.php

--- app/Helpers/Helpers.php
+++ app/Helpers/Helpers.php
@@ -2,9 +2,10 @@
 
 namespace App\Helpers;
 
-use Carbon\Carbon;
+use Illuminate\Support\Carbon;
 
-class Helpers{
+class Helpers
+{
     public static function calc_birthday($persons, $personindex)
     {
         $carbon_birthday = Carbon::createFromFormat('Y-m-d', $persons[$personindex]->birthday);
