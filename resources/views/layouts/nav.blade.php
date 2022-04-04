0f1d47c329f41060f76af12193e98f4e31640b4e
Caspar Brenneisen <caspar.brenneisen@fadendaten.ch>
2022-04-04 19:06:55 +0200

    Update nav.blade.php

--- resources/views/layouts/nav.blade.php
+++ resources/views/layouts/nav.blade.php
@@ -11,10 +11,11 @@
         <div class="collapse navbar-collapse" id="navbarSupportedContent">
             @if(!Auth::guest())
                 <!-- Left Side Of Navbar -->
-                <ul class="navbar-nav ms-auto">
+
+                <ul class="navbar-nav mr-auto">
                     <li class="nav-item">
                         <a class="nav-link {{ Request::is('overwatch/')}}" href="{{ url('/overwatch') }}">
-                            Übersicht
+                            Overwatch
                         </a>
                     </li>
 
