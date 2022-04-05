a06dac90efcb93e43d8167d90fb11234e9bc6b28
Caspar Brenneisen <caspar.brenneisen@fadendaten.ch>
2022-04-05 12:46:12 +0200

    Update edit.blade.php

--- resources/views/groups/edit.blade.php
+++ resources/views/groups/edit.blade.php
@@ -1,7 +1,7 @@
 @extends('layouts.app')
 
 @section('content')
-    <div class="col-12">
+    <div class="container">
         @if(session()->has('message'))
             <div class="alert alert-success">
                 {{ session()->get('message') }}
@@ -10,24 +10,22 @@
 
         <div class="card">
             <div class="card-header">
-                <h5 class="float-left">Gruppe bearbeiten</h5>
+                <h5 class="float-start">Gruppe bearbeiten</h5>
 
-                <a href="{{ route('groups') }}" class="float-right">Zurück zu Gruppen</a>
+                <a href="{{ route('groups') }}" class="float-end">Zurück zu Gruppen</a>
             </div>
             <div class="card-body">
                 {!! Form::open(array('route' => ['update-groups',$groups->id], 'method' => 'POST', 'role' => 'form', 'class' => 'needs-validation', 'enctype' => "multipart/form-data")) !!}
                 {!! csrf_field() !!}
 
-                <div class="form-group has-feedback row {{ $errors->has('group_name') ? ' has-error ' : '' }}">
+                <div class="row has-feedback {{ $errors->has('group_name') ? ' has-error ' : '' }}">
                     {!! Form::label('group_name', 'Gruppenname', array('class' => 'col-md-3 control-label')); !!}
                     <div class="col-md-9">
-                        <div class="input-group">
+                        <div class="input-group mb-3">
                             {!! Form::text('group_name', old('group_name', $groups->group_name ?? null), array('id' => 'group_name', 'class' => 'form-control', 'placeholder' => 'Gruppenname', 'required')) !!}
-                            <div class="input-group-append">
-                                <label class="input-group-text" for="group_name">
-                                    <i class="fa fa-group" aria-hidden="true"></i>
-                                </label>
-                            </div>
+                            <label class="input-group-text" for="group_name">
+                                <i class="fa fa-group" aria-hidden="true"></i>
+                            </label>
                         </div>
                         @if ($errors->has('group_name'))
                             <span class="help-block">
@@ -37,10 +35,10 @@
                     </div>
                 </div>
 
-                <div class="form-group has-feedback row {{ $errors->has('group_logo') ? ' has-error ' : '' }}">
+                <div class="row has-feedback {{ $errors->has('group_logo') ? ' has-error ' : '' }}">
                     {!! Form::label('group_logo', 'Gruppenlogo', array('class' => 'col-md-3 control-label')); !!}
                     <div class="col-md-9">
-                        <div class="input-group">
+                        <div class="input-group mb-3">
                             <input type="file" accept="image/*" id="group_logo" name="group_logo" />
                         </div>
                         @if ($errors->has('group_logo'))
@@ -51,7 +49,7 @@
                     </div>
                 </div>
 
-                {!! Form::button('Gruppe aktualisieren', array('class' => 'btn btn-success margin-bottom-1 mb-1 float-right','type' => 'submit' )) !!}
+                {!! Form::button('Gruppe aktualisieren', array('class' => 'btn btn-success col-12','type' => 'submit' )) !!}
                 {!! Form::close() !!}
             </div>
         </div>
