1dabd560f5c46e36fbcbfc087057c89e45e479fa
Caspar Brenneisen <caspar.brenneisen@fadendaten.ch>
2022-04-05 09:06:43 +0200

    Update users.blade.php

--- resources/views/users/users.blade.php
+++ resources/views/users/users.blade.php
@@ -1,7 +1,7 @@
 @extends('layouts.app')
 
 @section('content')
-    <div class="col-12">
+    <div class="container">
         @if(session()->has('message'))
             <div class="alert alert-success">
                 {{ session()->get('message') }}
@@ -34,9 +34,9 @@
 
         <div class="card">
             <div class="card-header">
-                <h5 class="float-left">Benutzer</h5>
+                <h5 class="float-start">Benutzer</h5>
 
-                <a href="{{  route('overwatch') }}" class="float-right">Zurück zu Overwatch</a>
+                <a href="{{  route('overwatch') }}" class="float-end">Zurück zu Overwatch</a>
             </div>
             <div class="card-body table-responsive">
                 <table class="table table-hover">
