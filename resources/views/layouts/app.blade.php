b412037a0e9e3d7962af43606b09af77c9f8a170
Caspar Brenneisen <caspar.brenneisen@fadendaten.ch>
2022-04-06 09:17:22 +0200

    Update app.blade.php

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -11,7 +11,6 @@
 
     <!-- Scripts -->
     <script src="{{ asset('js/app.js') }}" defer></script>
-    <script src="https://code.jquery.com/ui/1.13.1/jquery-ui.js" defer></script>
 
     <!-- Fonts -->
     <link rel="dns-prefetch" href="//fonts.gstatic.com" />
