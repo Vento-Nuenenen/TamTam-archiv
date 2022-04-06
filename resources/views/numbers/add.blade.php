cb31ec6d293b55733e4717e9d5c4d41327a51646
Caspar Brenneisen <caspar.brenneisen@fadendaten.ch>
2022-04-06 09:17:25 +0200

    Update add.blade.php

--- resources/views/numbers/add.blade.php
+++ resources/views/numbers/add.blade.php
@@ -1,7 +1,7 @@
 @extends('layouts.app')
 
 @section('content')
-    <div class="col-12">
+    <div class="container">
         @if(session()->has('message'))
             <div class="alert alert-success">
                 {{ session()->get('message') }}
@@ -10,24 +10,22 @@
 
         <div class="card">
             <div class="card-header">
-                <h5 class="float-left">Nummer erstellen</h5>
+                <h5 class="float-start">Nummer erstellen</h5>
 
-                <a href="{{  route('numbers') }}" class="float-right">Zurück zu Nummern</a>
+                <a href="{{  route('numbers') }}" class="float-end">Zurück zu Nummern</a>
             </div>
             <div class="card-body">
                 {!! Form::open(array('route' => 'store-numbers', 'method' => 'POST', 'role' => 'form', 'class' => 'needs-validation')) !!}
                 {!! csrf_field() !!}
 
-                <div class="form-group has-feedback row {{ $errors->has('number_name') ? ' has-error ' : '' }}">
+                <div class="row has-feedback {{ $errors->has('number_name') ? ' has-error ' : '' }}">
                     {!! Form::label('number_name', 'Nummernbezeichnung', array('class' => 'col-md-3 control-label')); !!}
                     <div class="col-md-9">
-                        <div class="input-group">
+                        <div class="input-group mb-3">
                             {!! Form::text('number_name', NULL, array('id' => 'number_name', 'class' => 'form-control', 'placeholder' => 'Nummernbezeichnung', 'required')) !!}
-                            <div class="input-group-append">
-                                <label class="input-group-text" for="group_name">
-                                    <i class="fa fa-heart" aria-hidden="true"></i>
-                                </label>
-                            </div>
+                            <label class="input-group-text" for="group_name">
+                                <i class="fa fa-heart" aria-hidden="true"></i>
+                            </label>
                         </div>
                         @if ($errors->has('number_name'))
                             <span class="help-block">
@@ -37,16 +35,14 @@
                     </div>
                 </div>
 
-                <div class="form-group has-feedback row {{ $errors->has('number') ? ' has-error ' : '' }}">
+                <div class="row has-feedback {{ $errors->has('number') ? ' has-error ' : '' }}">
                     {!! Form::label('number', 'Telefonnummer', array('class' => 'col-md-3 control-label')); !!}
                     <div class="col-md-9">
-                        <div class="input-group">
+                        <div class="input-group mb-3">
                             {!! Form::text('number', NULL, array('id' => 'number', 'class' => 'form-control', 'placeholder' => 'Telefonnummer', 'required')) !!}
-                            <div class="input-group-append">
-                                <label class="input-group-text" for="group_name">
-                                    <i class="fa fa-phone" aria-hidden="true"></i>
-                                </label>
-                            </div>
+                            <label class="input-group-text" for="group_name">
+                                <i class="fa fa-phone" aria-hidden="true"></i>
+                            </label>
                         </div>
                         @if ($errors->has('number'))
                             <span class="help-block">
@@ -56,7 +52,7 @@
                     </div>
                 </div>
 
-                {!! Form::button('Nummer erstellen', array('class' => 'btn btn-success margin-bottom-1 mb-1 float-right','type' => 'submit' )) !!}
+                {!! Form::button('Nummer erstellen', array('class' => 'btn btn-success col-12','type' => 'submit' )) !!}
                 {!! Form::close() !!}
             </div>
         </div>
