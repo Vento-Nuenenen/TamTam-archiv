85fdae0e81dc8fc44a2c80c8bd6b523f4cefcc39
Vento <github@brenneisen.me>
2022-09-14 20:25:58 +0200

    Update bootstrap.js

--- resources/js/bootstrap.js
+++ resources/js/bootstrap.js
@@ -1,17 +1,9 @@
-window._ = require('lodash');
+import _ from 'lodash';
+window._ = _;
 
-try {
-    require('bootstrap');
-} catch (e) {
-    console.error("Error: " + e);
-}
+import 'bootstrap';
 
-/**
- * We'll load the axios HTTP library which allows us to easily issue requests
- * to our Laravel back-end. This library automatically handles sending the
- * CSRF token as a header based on the value of the "XSRF" token cookie.
- */
-
-window.axios = require('axios');
+import axios from 'axios';
+window.axios = axios;
 
 window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
