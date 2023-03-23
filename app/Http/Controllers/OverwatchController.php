7ab23ee01b7ab217075cbe239b0aa0bcce75db20
Vento <github@brenneisen.me>
2023-03-23 20:50:31 +0100

    Update OverwatchController.php

--- app/Http/Controllers/OverwatchController.php
+++ app/Http/Controllers/OverwatchController.php
@@ -7,7 +7,7 @@ use Illuminate\Contracts\View\View;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
 
-class OverwatchController
+class OverwatchController extends Controller
 {
     /**
      * Display a listing of the resource.
@@ -16,7 +16,7 @@ class OverwatchController
      */
     public function index(Request $request)
     {
-        if ($request->input('barcode') != null) {
+        if($request->input('barcode') != null) {
             $barcode = $request->input('barcode');
 
             $tns = DB::select("SELECT participations.*, points.*, groups.*, GROUP_CONCAT(points.points) AS points,
@@ -25,15 +25,15 @@ class OverwatchController
   			    LEFT JOIN `groups` ON `participations`.`FK_GRP` = `groups`.`id` WHERE `participations`.`barcode` LIKE $barcode
  				GROUP BY participations.id;");
 
-            foreach ($tns as $tn) {
+            foreach($tns as $tn) {
                 $balance = 0;
 
-                if (! empty($tn->points) || ! empty($tn->additions)) {
+                if(!empty($tn->points) || !empty($tn->additions)) {
                     $points = explode(',', $tn->points);
                     $additions = explode(',', $tn->additions);
 
-                    for ($i = 0; $i < count($points); $i++) {
-                        if ($additions[$i] == 1) {
+                    for($i = 0; $i < count($points); $i++) {
+                        if($additions[$i] == 1) {
                             $balance += $points[$i];
                         } else {
                             $balance -= $points[$i];
@@ -47,12 +47,12 @@ class OverwatchController
             $tns = $tns[0] ?? null;
 
             return view('overwatch.overwatch', ['tn' => $tns]);
-        } elseif ($request->input('tableorder') != null) {
+        } elseif($request->input('tableorder') != null) {
             $users = DB::table('participations')->inRandomOrder()->get();
 
             $j = 0;
 
-            foreach ($users as $user) {
+            foreach($users as $user) {
                 $j++;
                 DB::table('participations')->where('id', '=', $user->id)->update(['seat_number' => $j]);
             }
@@ -60,14 +60,14 @@ class OverwatchController
             session()->put('message', 'Tischordnung wurde erfolgreich generiert!');
 
             return view('overwatch.overwatch');
-        } elseif ($request->input('grouping') != null) {
+        } elseif($request->input('grouping') != null) {
             $groups = DB::table('groups')->get();
             $groups_count = count($groups);
             $users = DB::table('participations')->inRandomOrder()->get();
             $j = 1;
 
-            foreach ($users as $user) {
-                if ($j <= $groups_count) {
+            foreach($users as $user) {
+                if($j <= $groups_count) {
                     DB::table('participations')->where('id', '=', $user->id)->update(['FK_GRP' => $j]);
                     $j++;
                 } else {
