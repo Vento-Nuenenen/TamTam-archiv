f13b1e6f3a6012af2a3ce35c72b159d60ecce7b5
Vento <github@brenneisen.me>
2023-03-23 20:31:21 +0100

    Update Handler.php

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -9,16 +9,25 @@ use Throwable;
 class Handler extends ExceptionHandler
 {
     /**
+     * A list of exception types with their corresponding custom log levels.
+     *
+     * @var array<class-string<\Throwable>, \Psr\Log\LogLevel::*>
+     */
+    protected $levels = [
+        //
+    ];
+
+    /**
      * A list of the exception types that are not reported.
      *
-     * @var array<int, class-string<Throwable>>
+     * @var array<int, class-string<\Throwable>>
      */
     protected $dontReport = [
         //
     ];
 
     /**
-     * A list of the inputs that are never flashed for validation exceptions.
+     * A list of the inputs that are never flashed to the session on validation exceptions.
      *
      * @var array<int, string>
      */
@@ -30,10 +39,8 @@ class Handler extends ExceptionHandler
 
     /**
      * Register the exception handling callbacks for the application.
-     *
-     * @return void
      */
-    public function register()
+    public function register(): void
     {
         $this->reportable(function (Throwable $e) {
             //
@@ -50,9 +57,9 @@ class Handler extends ExceptionHandler
      */
     public function report(Throwable $exception)
     {
-        if (App::environment('production')) {
-            if ($this->shouldReport($exception)) {
-                $airbrakeNotifier = \App::make('Airbrake\Notifier');
+        if(App::environment('production')) {
+            if($this->shouldReport($exception)) {
+                $airbrakeNotifier = App::make('Airbrake\Notifier');
                 $airbrakeNotifier->notify($exception);
             }
         }
