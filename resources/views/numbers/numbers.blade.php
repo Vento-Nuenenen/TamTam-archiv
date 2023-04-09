3451615011aba71acf1bf2dc25f02e2cf7ee2b79
Vento <github@brenneisen.me>
2023-04-10 00:11:54 +0200

    Update numbers.blade.php

--- resources/views/numbers/numbers.blade.php
+++ resources/views/numbers/numbers.blade.php
@@ -56,3 +56,45 @@
         </div>
     </div>
 @endsection
+
+@section('script')
+    <script type="text/javascript">
+        $(function () {
+            $( "#tablecontents" ).sortable({
+                items: "tr",
+                cursor: 'move',
+                opacity: 0.6,
+                update: function() {
+                    sendOrderToServer();
+                }
+            });
+            function sendOrderToServer() {
+                var order = [];
+                var token = $('meta[name="csrf-token"]').attr('content');
+                $('tr').each(function(index, element) {
+                    order.push({
+                        id: $(this).attr('data-id'),
+                        position: index+1
+                    });
+                });
+                $.ajax({
+                    type: "POST",
+                    dataType: "json",
+                    url: "{{ url('numbers/sort') }}",
+                    data: {
+                        order: order,
+                        _token: token
+                    },
+                    success: function(response) {
+                        alert(response.status);
+                        if (response.status == "success") {
+                            console.log(response);
+                        } else {
+                            console.log(response);
+                        }
+                    }
+                });
+            }
+        });
+    </script>
+@endsection
