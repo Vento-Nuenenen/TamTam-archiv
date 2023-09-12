14d5312f0db382cc258ddacfad03ddb1db24a86b
Vento <github@brenneisen.me>
2023-09-12 20:40:24 +0200

    Create UsersTableSeeder.php

---
+++ database/seeders/UsersTableSeeder.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class UsersTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        // Seed test admin
+        $seededAdminEmail = 'admin@tab.ch';
+        $user = User::where('email', '=', $seededAdminEmail)->first();
+        if ($user === null) {
+            $user = User::create(['scout_name' => 'Admin', 'first_name' => 'Admin', 'last_name' => 'Admin', 'email' => $seededAdminEmail, 'password' => Hash::make('password')]);
+            $user->save();
+        }
+
+        // Seed test user
+        $user = User::where('email', '=', 'user@user.com')->first();
+        if ($user === null) {
+            $user = User::create([
+                'scout_name'                     => 'Vento',
+                'first_name'                     => 'Caspar',
+                'last_name'                      => 'Brenneisen',
+                'email'                          => 'caspar.brenneisen@protonmail.ch',
+                'password'                       => Hash::make('password'),
+            ]);
+            $user->save();
+        }
+    }
+}
