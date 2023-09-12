6ebb3fecacdf462e8baa18b178506d1cfd12f025
Vento <github@brenneisen.me>
2023-09-12 20:33:57 +0200

    Create 2023_09_12_183310_create_group_table.php

---
+++ database/migrations/2023_09_12_183310_create_group_table.php
@@ -0,0 +1,29 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('group', function (Blueprint $table) {
+            $table->id();
+            $table->string('group_name');
+            $table->string('logo_file_name')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('group');
+    }
+};
