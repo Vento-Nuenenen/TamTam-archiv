7b1c1fb91d0e8a2233206df521a8b09836d62592
Vento <github@brenneisen.me>
2023-09-12 20:35:48 +0200

    Create 2023_09_12_183529_create_points_table.php

---
+++ database/migrations/2023_09_12_183529_create_points_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('points', function (Blueprint $table) {
+            $table->id();
+            $table->string('reason')->nullable();
+            $table->integer('points');
+            $table->boolean('is_addition');
+            $table->bigInteger('FK_PRT')->unsigned()->index();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('points');
+    }
+};
