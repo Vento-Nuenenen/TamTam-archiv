878cd742cfcb5820f30e19e9afe2f19f2ae92c1c
Vento <github@brenneisen.me>
2023-10-21 17:28:06 +0200

    Update BroadcastServiceProvider.php

--- app/Providers/BroadcastServiceProvider.php
+++ app/Providers/BroadcastServiceProvider.php
@@ -2,6 +2,7 @@
 
 namespace App\Providers;
 
+use Illuminate\Support\Facades\Broadcast;
 use Illuminate\Support\ServiceProvider;
 
 class BroadcastServiceProvider extends ServiceProvider
@@ -11,6 +12,8 @@ class BroadcastServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        //
+        Broadcast::routes();
+
+        require base_path('routes/channels.php');
     }
 }
